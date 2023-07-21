3cc3231c18896c3c4f2350f596a754b7b4afd71d
NurislamAituarov <nurislamajtuarov@gmail.com>
2023-07-21 14:30:52 +0600

    e0d83fe

---
+++ _nuxt/static/1689927250/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fapp-comments\u002F_nuxt\u002Fstatic\u002F1689927250",layout:"default",error:a,serverRendered:true,routePath:"\u002F",config:{_app:{basePath:"\u002Fapp-comments\u002F",assetsPath:"\u002Fapp-comments\u002F_nuxt\u002F",cdnURL:a}}}}(null));
